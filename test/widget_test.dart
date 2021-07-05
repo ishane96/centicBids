c41e6a5e94571f51ba53df6bde2dfcdb9124daec
Achintha ishane <39967945+ishane96@users.noreply.github.com>
2021-07-05 23:39:29 +0530

    Update widget_test.dart
    test class updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,21 +10,31 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:centic_bids/main.dart';
 
+import '../lib/Utilities/Validators.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  test('Empty Name Test', () {
+    var result = AuthValidator.validateName('');
+    expect(result, 'Please fill name');
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test('Empty Email Test', () {
+    var result = AuthValidator.validateEmail('');
+    expect(result, 'Please enter your email');
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Empty Password Test', () {
+    var result = AuthValidator.validatePassword('');
+    expect(result, 'Please fill password');
+  });
+
+  test('Confirm Password Test', () {
+    var result = AuthValidator.validateRetypePassword('', '');
+    expect(result, 'Please fill same password');
   });
 }
